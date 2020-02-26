da2016062a71b94e1318ef539dca2187fc7c9cf0
Bala Dasoji <bala.dasoji@gmail.com>
2020-02-26 17:26:43 +0100

    Removed the phanton string and updated scripts to sync instead of copy to s3

--- website.dasoji.net/update-website.sh
+++ website.dasoji.net/update-website.sh
@@ -1,5 +1,5 @@
-aws s3 cp ../docs/public/ s3://results.dasoji.net/ --recursive
-aws s3 cp ../docs/admin/ s3://raceadmin.dasoji.net/ --recursive
+aws s3 sync ../docs/public/ s3://results.dasoji.net/ 
+aws s3 sync ../docs/admin/ s3://raceadmin.dasoji.net/ 
 aws s3 website s3://results.dasoji.net/ --index-document index.html --error-document error.html
 aws s3 website s3://raceadmin.dasoji.net/ --index-document index.html --error-document error.html
 aws s3api put-bucket-policy --bucket results.dasoji.net --policy file://results-policy.json
--- website.rulleraes.dk/update-website.sh
+++ website.rulleraes.dk/update-website.sh
@@ -1,5 +1,5 @@
-aws s3 cp ../docs/public/ s3://results.rulleraes.dk/ --recursive
-aws s3 cp ../docs/admin/ s3://raceadmin.rulleraes.dk/ --recursive
+aws s3 sync ../docs/public/ s3://results.rulleraes.dk/ 
+aws s3 sync ../docs/admin/ s3://raceadmin.rulleraes.dk/ 
 aws s3 website s3://results.rulleraes.dk/ --index-document index.html --error-document error.html
 aws s3 website s3://raceadmin.rulleraes.dk/ --index-document index.html --error-document error.html
 aws s3api put-bucket-policy --bucket results.rulleraes.dk --policy file://results-policy.json
