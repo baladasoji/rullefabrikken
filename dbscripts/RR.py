014cbdd2edaa55695ee86336fd613789ee606951
Bala Dasoji <bala.dasoji@gmail.com>
2020-02-28 05:54:54 +0100

    New functions for lambdas

--- dbscripts/RR.py
+++ dbscripts/RR.py
@@ -10,7 +10,7 @@ def convert_csv_to_json_list(file):
           '''print (row)'''
           data = {}
           for k in row.keys():
-            if ( k == 'id' or k == 'eventid' or k=='bib' or k=='age'):
+            if ( k == 'id' or k == 'eventid' or k=='bib' or k=='age' or k=='points'):
                data[k] = int(row[k])
             else:
                data[k] = row[k]
@@ -84,10 +84,57 @@ def cleanup_Event(eventid):
    clean_Table('RRRaces',eventid)
    clean_Table('RRPlayers',eventid)
 
+def players_by_group(groupname):
+   dynamodb = boto3.resource('dynamodb')
+   rrplayersTab = dynamodb.Table('RRPlayers')
+   fe = Key('agegroup').eq(groupname)
+   rrplayersResponse = rrplayersTab.scan( FilterExpression=fe )
+   print rrplayersResponse['Items']
+
+def players_groups():
+   dynamodb = boto3.resource('dynamodb')
+   rrplayersTab = dynamodb.Table('RRPlayers')
+   pe = "agegroup"
+   rrplayersResponse = rrplayersTab.scan( ProjectionExpression=pe )
+   print rrplayersResponse
+   lst=[]
+   f=lambda y: y.values()[0]
+   x=list(dict.fromkeys([ f(y)  for y in rrplayersResponse['Items'] ]))
+   return {'Items' : x}
+
+def reset_points():
+   dynamodb = boto3.resource('dynamodb')
+   rrplayersTab = dynamodb.Table('RRPlayers')
+   rrplayersResponse = rrplayersTab.scan( ProjectionExpression='#k,#s', ExpressionAttributeNames={ '#k' : 'id', '#s' : 'sortkey' })
+   for item in rrplayersResponse['Items']:
+    rrplayersTab.update_item(Key=item, UpdateExpression= 'SET #pnt = :point', ExpressionAttributeNames= {"#pnt" : "points"},  ExpressionAttributeValues= { ":point": 0 } )
+
+def update_results(eventid):
+   dynamodb = boto3.resource('dynamodb')
+   rrresultsTab = dynamodb.Table('RRResults')
+   rrresultsResponse = rrresultsTab.scan()
+   results = rrresultsResponse['Items']
+   rrplayersTab = dynamodb.Table('RRPlayers')
+   rrplayersResponse = rrplayersTab.scan()
+   players = rrplayersResponse['Items']
+   pdict={}
+   for i in players:
+    pdict[i.get('id')]=0
+   for r in results:
+    res=r['result']
+    for rr in res:
+        pdict[rr.get('playerid')]+= rr.get('points')
+   for i in players:
+     rrplayersTab.update_item(Key={'id':i['id'] }, UpdateExpression= 'SET #pnt = :point', ExpressionAttributeNames= {"#pnt" : "points"},  ExpressionAttributeValues= { ":point": pdict.get(i['id']) } )
+
 if __name__ == '__main__':
-   update_race_status(2,'finished')
+   print players_groups()
    '''
+   players_by_group('maxi')
+   reset_points()
+   update_results(1)
    clean_and_reload_RR()
+   update_race_status(2,'finished')
    count_Table('RRPlayers')
    cleanup_Event(1);
    my_test()
